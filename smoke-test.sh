59c8ad45cb597908d01e593ccb020300ac2f68ef
David Joy <davjoy@gmail.com>
2024-06-27 16:43:16 -0400

    refactor: moving the frontend-build example app into a subdirectory
    Subsequent commit will fix paths and the name of the app in package.json.
    
    The point of doing this is to get it out of the way for when I want to add commits from other repositories, such as frontend-platform.  I want to be able to do that without dealing with conflicts wherever possible.

--- smoke-test.sh
+++ smoke-test.sh
@@ -2,10 +2,10 @@
 # https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
 set -e
 node ./bin/fedx-scripts jest --coverage plugins
-cd example
+cd cli/test-app
 npm install
 npm run lint
 npm run test
 npm run build
 npm run babel
-cd ../
+cd ../../
