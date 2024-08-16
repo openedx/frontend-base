0f8bd546a8efb8d612dba1d18e2ee8a553db8461
David Joy <davjoy@gmail.com>
2024-08-16 14:56:39 -0400

    test: removing warning about missing locales.

--- shell/header/learning-header/LearningHeader.test.jsx
+++ shell/header/learning-header/LearningHeader.test.jsx
@@ -6,7 +6,6 @@ import {
   AppProvider,
   configureAuth, configureI18n, configureLogging, getConfig, IntlProvider, mergeConfig, MockAuthService
 } from '../../../runtime';
-import appMessages from '../i18n';
 import { LearningHeader as Header } from '../index';
 
 class MockLoggingService {
@@ -59,7 +58,24 @@ function initializeMockApp() {
   configureI18n({
     config: getConfig(),
     loggingService,
-    messages: [appMessages],
+    messages: [{
+      ar: {},
+      // NOTE: 'en' is not included in this list intentionally, since it's the fallback.
+      'es-419': {},
+      fa: {},
+      'fa-ir': {},
+      fr: {},
+      'zh-cn': {},
+      ca: {},
+      he: {},
+      id: {},
+      'ko-kr': {},
+      pl: {},
+      'pt-br': {},
+      ru: {},
+      th: {},
+      uk: {},
+    }],
   });
 
   return { loggingService, authService };
