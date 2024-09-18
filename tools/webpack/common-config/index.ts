a0c2758e530e21e256d3674b425dcf25925b99e4
David Joy <davjoy@gmail.com>
2024-09-18 19:00:57 -0400

    fix: exporting common-config helpers from the index file
    Prior commits will break without this, as we’re importing them into the webpack configs from this file.  Oops.

--- tools/webpack/common-config/index.ts
+++ tools/webpack/common-config/index.ts
@@ -1,1 +1,5 @@
+export { default as getFileLoaderRules } from './all/getFileLoaderRules';
+export { default as getIgnoreWarnings } from './all/getIgnoreWarnings';
 export { default as getImageMinimizer } from './all/getImageMinimizer';
+export { default as getDevServer } from './dev/getDevServer';
+export { default as getHtmlWebpackPlugin } from './site/getHtmlWebpackPlugin';
