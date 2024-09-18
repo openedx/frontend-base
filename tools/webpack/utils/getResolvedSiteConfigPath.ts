ea08871a574a84669c5f0f61673d061feb522ad3
David Joy <davjoy@gmail.com>
2024-09-18 18:28:56 -0400

    feat: adding a helper to resolve site configs for webpack
    Prior to this function, site config files were hard coded into the webpack configs.  This allows them to be defaulted and overridden via an environment variable supplied on the command line: SITE_CONFIG_PATH.
    
    The SITE_CONFIG_PATH can be relative to the current working directory (process.cwd()) or an absolute path.

---
+++ tools/webpack/utils/getResolvedSiteConfigPath.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function getResolvedSiteConfigPath(defaultFilename: string) {
+  let siteConfigPath = process.env.SITE_CONFIG_PATH;
+
+  if (siteConfigPath !== undefined) {
+    if (fs.existsSync(siteConfigPath)) {
+      // If this is a valid absolute path, return it.
+      return siteConfigPath;
+    }
+    const absolutePath = path.resolve(process.cwd(), siteConfigPath);
+    if (fs.existsSync(absolutePath)) {
+      // If it's a valid absolute path when joined with the current working directory, return that.
+      return absolutePath;
+    }
+  }
+  // Otherwise, return our default site config file path.
+  const defaultPath = path.resolve(process.cwd(), defaultFilename);
+  if (fs.existsSync(defaultPath)) {
+    return defaultPath;
+  }
+
+  // They supplied something but we can't figure out what it is.  Exit with an error.
+  if (siteConfigPath !== undefined) {
+    console.error(`Invalid site config path (${siteConfigPath} specified as an environment variable. Aborting.`);
+  } else {
+    console.error(`Default site config file (${defaultPath}) does not exist. Aborting.`)
+  }
+  process.exit(1);
+}
