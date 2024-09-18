7621d01adad26116f9b65fbab1b850a54e83fc78
David Joy <davjoy@gmail.com>
2024-09-18 18:59:30 -0400

    refactor: extracting file-loader rules tino getFileLoaderRules
    They’re identical in all webpack configs.

--- tools/webpack/webpack.build.module.config.ts
+++ tools/webpack/webpack.build.module.config.ts
@@ -11,6 +11,7 @@ import { Configuration, WebpackError } from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import {
+  getFileLoaderRules,
   getIgnoreWarnings,
   getImageMinimizer,
 } from './common-config';
@@ -132,25 +133,7 @@ const config: Configuration = {
           },
         ],
       },
-      // Webpack, by default, uses the url-loader for images and fonts that are required/included by
-      // files it processes, which just base64 encodes them and inlines them in the javascript
-      // bundles. This makes the javascript bundles ginormous and defeats caching so we will use the
-      // file-loader instead to copy the files directly to the output directory.
-      {
-        test: /\.(woff2?|ttf|svg|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: require.resolve('file-loader'),
-      },
-      {
-        test: /favicon.ico$/,
-        loader: require.resolve('file-loader'),
-        options: {
-          name: '[name].[ext]', // <-- retain original file name
-        },
-      },
-      {
-        test: /\.(jpe?g|png|gif)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: require.resolve('file-loader'),
-      },
+      ...getFileLoaderRules(),
     ],
   },
   // Extract common modules among all chunks to one common chunk and extract the Webpack runtime to a single runtime chunk.
--- tools/webpack/webpack.dev.config.ts
+++ tools/webpack/webpack.dev.config.ts
@@ -13,6 +13,7 @@ import RemoveEmptyScriptsPlugin from 'webpack-remove-empty-scripts';
 
 import {
   getDevServer,
+  getFileLoaderRules,
   getHtmlWebpackPlugin,
   getIgnoreWarnings,
   getImageMinimizer,
@@ -147,25 +148,7 @@ const config: Configuration = {
           },
         ],
       },
-      // Webpack, by default, uses the url-loader for images and fonts that are required/included by
-      // files it processes, which just base64 encodes them and inlines them in the javascript
-      // bundles. This makes the javascript bundles ginormous and defeats caching so we will use the
-      // file-loader instead to copy the files directly to the output directory.
-      {
-        test: /\.(woff2?|ttf|svg|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: require.resolve('file-loader'),
-      },
-      {
-        test: /favicon.ico$/,
-        loader: require.resolve('file-loader'),
-        options: {
-          name: '[name].[ext]', // <-- retain original file name
-        },
-      },
-      {
-        test: /\.(jpe?g|png|gif)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: require.resolve('file-loader'),
-      },
+      ...getFileLoaderRules(),
     ],
   },
   optimization: {
