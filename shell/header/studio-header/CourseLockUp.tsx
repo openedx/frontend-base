b4b70f714f6487e43999d2aab430478317820a7c
David Joy <davjoy@gmail.com>
2024-09-24 10:26:22 -0400

    refactor: removing propTypes from CourseLockUp

--- shell/header/studio-header/CourseLockUp.tsx
+++ shell/header/studio-header/CourseLockUp.tsx
@@ -2,52 +2,41 @@ import {
   OverlayTrigger,
   Tooltip,
 } from '@openedx/paragon';
-import PropTypes from 'prop-types';
-import { injectIntl, intlShape } from '../../../runtime';
+import { useIntl } from '../../../runtime';
 import messages from './messages';
 
-const CourseLockUp = ({
+interface CourseLockUpProps {
+  number: string,
+  org: string,
+  title: string,
+  outlineLink: string,
+}
+
+export default function CourseLockUp({
   outlineLink,
   org,
   number,
   title,
-  // injected
-  intl,
-}) => (
-  <OverlayTrigger
-    placement="bottom"
-    overlay={(
-      <Tooltip id="course-lock-up">
-        {title}
-      </Tooltip>
+}: CourseLockUpProps) {
+  const intl = useIntl();
+  return (
+    <OverlayTrigger
+      placement="bottom"
+      overlay={(
+        <Tooltip id="course-lock-up">
+          {title}
+        </Tooltip>
     )}
-  >
-    <a
-      className="course-title-lockup mr-2"
-      href={outlineLink}
-      aria-label={intl.formatMessage(messages['header.label.courseOutline'])}
-      data-testid="course-lock-up-block"
     >
-      <span className="d-block small m-0 text-gray-800" data-testid="course-org-number">{org} {number}</span>
-      <span className="d-block m-0 font-weight-bold text-gray-800" data-testid="course-title">{title}</span>
-    </a>
-  </OverlayTrigger>
-);
-
-CourseLockUp.propTypes = {
-  number: PropTypes.string,
-  org: PropTypes.string,
-  title: PropTypes.string,
-  outlineLink: PropTypes.string,
-  // injected
-  intl: intlShape.isRequired,
-};
-
-CourseLockUp.defaultProps = {
-  number: null,
-  org: null,
-  title: null,
-  outlineLink: null,
-};
-
-export default injectIntl(CourseLockUp);
+      <a
+        className="course-title-lockup mr-2"
+        href={outlineLink}
+        aria-label={intl.formatMessage(messages['header.label.courseOutline'])}
+        data-testid="course-lock-up-block"
+      >
+        <span className="d-block small m-0 text-gray-800" data-testid="course-org-number">{org} {number}</span>
+        <span className="d-block m-0 font-weight-bold text-gray-800" data-testid="course-title">{title}</span>
+      </a>
+    </OverlayTrigger>
+  );
+}
