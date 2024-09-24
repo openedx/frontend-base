fee620fc472a94a62741177461acfced43eb578d
David Joy <davjoy@gmail.com>
2024-09-24 10:57:10 -0400

    refactor: removing propTypes from AnonymousUserMenu

--- shell/header/learning-header/AnonymousUserMenu.tsx
+++ shell/header/learning-header/AnonymousUserMenu.tsx
@@ -1,30 +1,28 @@
 import { Button } from '@openedx/paragon';
 import {
-  getConfig, getLoginRedirectUrl, injectIntl, intlShape
+  getConfig, getLoginRedirectUrl,
+  useIntl
 } from '../../../runtime';
 
 import messages from './messages';
 
-const AnonymousUserMenu = ({ intl }) => (
-  <div>
-    <Button
-      className="mr-3"
-      variant="outline-primary"
-      href={`${getConfig().LMS_BASE_URL}/register?next=${encodeURIComponent(global.location.href)}`}
-    >
-      {intl.formatMessage(messages.registerSentenceCase)}
-    </Button>
-    <Button
-      variant="primary"
-      href={`${getLoginRedirectUrl(global.location.href)}`}
-    >
-      {intl.formatMessage(messages.signInSentenceCase)}
-    </Button>
-  </div>
-);
-
-AnonymousUserMenu.propTypes = {
-  intl: intlShape.isRequired,
-};
-
-export default injectIntl(AnonymousUserMenu);
+export default function AnonymousUserMenu() {
+  const intl = useIntl();
+  return (
+    <div>
+      <Button
+        className="mr-3"
+        variant="outline-primary"
+        href={`${getConfig().LMS_BASE_URL}/register?next=${encodeURIComponent(global.location.href)}`}
+      >
+        {intl.formatMessage(messages.registerSentenceCase)}
+      </Button>
+      <Button
+        variant="primary"
+        href={`${getLoginRedirectUrl(global.location.href)}`}
+      >
+        {intl.formatMessage(messages.signInSentenceCase)}
+      </Button>
+    </div>
+  );
+}
