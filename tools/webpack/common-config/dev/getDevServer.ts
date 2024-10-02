95260a8008e32499724b2f761b8d26baffff5e59
David Joy <davjoy@gmail.com>
2024-10-02 14:01:17 -0400

    refactor: sorting dev server fields

--- tools/webpack/common-config/dev/getDevServer.ts
+++ tools/webpack/common-config/dev/getDevServer.ts
@@ -5,26 +5,26 @@ import getPublicPath from "../../utils/getPublicPath";
 
 export default function getDevServer(): Configuration {
   return {
-    host: '0.0.0.0',
-    port: process.env.PORT || 8080,
+    // Setting the Access-Control-Allow-Origin header is required to get module federation to work
+    // locally.
+    devMiddleware: {
+      publicPath: getPublicPath(),
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     historyApiFallback: {
       index: path.join(getPublicPath(), 'index.html'),
       disableDotRule: true,
     },
-    // Setting the Access-Control-Allow-Origin header is required to get module federation to work
-    // locally.
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+    host: '0.0.0.0',
+    hot: true,
+    port: process.env.PORT || 8080,
     // Enable hot reloading server. It will provide WDS_SOCKET_PATH endpoint
     // for the WebpackDevServer client so it can learn when the files were
     // updated. The WebpackDevServer client is included as an entry point
     // in the webpack development configuration. Note that only changes
     // to CSS are currently hot reloaded. JS changes will refresh the browser.
-    hot: true,
     webSocketServer: 'ws',
-    devMiddleware: {
-      publicPath: getPublicPath(),
-    },
   }
 }
