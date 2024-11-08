9d7abc78483aa06c203c342e16c3335324f5e13e
David Joy <davjoy@gmail.com>
2024-11-08 14:49:44 -0500

    refactor: organizing site.config.dev.shell.tsx a bit
    Helps to realize very little of it is ‘common’ - most is about APIs or specific frontend modules.

--- shell/site.config.dev.shell.tsx
+++ shell/site.config.dev.shell.tsx
@@ -91,25 +91,32 @@ const config: ProjectSiteConfig = {
   //   }
   // },
 
+  // General
   APP_ID: 'shell',
-  ACCOUNT_PROFILE_URL: 'http://apps.local.openedx.io:1995',
-  ACCOUNT_SETTINGS_URL: 'http://apps.local.openedx.io:1997',
   BASE_URL: 'http://apps.local.openedx.io:8080',
   ENVIRONMENT: EnvironmentTypes.DEVELOPMENT,
-  FAVICON_URL: 'https://edx-cdn.org/v3/default/favicon.ico',
+  SITE_NAME: 'My Open edX Site',
+
+  // Plugin page URLs
+  ACCOUNT_PROFILE_URL: 'http://apps.local.openedx.io:1995',
+  ACCOUNT_SETTINGS_URL: 'http://apps.local.openedx.io:1997',
   LEARNER_DASHBOARD_URL: 'http://local.openedx.io:8000/dashboard',
   LEARNING_BASE_URL: 'http://apps.local.openedx.io:2000',
-  LMS_BASE_URL: 'http://local.openedx.io:8000',
   LOGIN_URL: 'http://local.openedx.io:8000/login',
+  ORDER_HISTORY_URL: 'http://apps.local.openedx.io:1996/orders',
+  STUDIO_BASE_URL: 'http://studio.local.openedx.io:8001',
+  MARKETING_SITE_BASE_URL: 'http://local.openedx.io:8000',
+  LOGOUT_URL: 'http://local.openedx.io:8000/logout',
+
+  // API URLs
+  LMS_BASE_URL: 'http://local.openedx.io:8000',
+  MFE_CONFIG_API_URL: 'http://apps.local.openedx.io:8080/api/mfe_config/v1',
+
+  // Brand URLs
+  FAVICON_URL: 'https://edx-cdn.org/v3/default/favicon.ico',
   LOGO_TRADEMARK_URL: 'https://edx-cdn.org/v3/default/logo-trademark.svg',
   LOGO_URL: 'https://edx-cdn.org/v3/default/logo.svg',
   LOGO_WHITE_URL: 'https://edx-cdn.org/v3/default/logo-white.svg',
-  LOGOUT_URL: 'http://local.openedx.io:8000/logout',
-  MARKETING_SITE_BASE_URL: 'http://local.openedx.io:8000',
-  MFE_CONFIG_API_URL: 'http://apps.local.openedx.io:8080/api/mfe_config/v1',
-  ORDER_HISTORY_URL: 'http://apps.local.openedx.io:1996/orders',
-  SITE_NAME: 'My Open edX Site',
-  STUDIO_BASE_URL: 'http://studio.local.openedx.io:8001',
 };
 
 export default config;
