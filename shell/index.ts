08007bfa6e651ae0bce4df56c386b0ec14ee4ef8
David Joy <davjoy@gmail.com>
2024-12-10 17:59:43 -0500

    fix: exporting many more members from the shell and types

---
+++ shell/index.ts
@@ -0,0 +1,7 @@
+export { default as DefaultLayout } from './DefaultLayout';
+export { default as DefaultMain } from './DefaultMain';
+export { default as defaultShellConfig } from './defaultShellConfig';
+export { Footer, defaultFooterConfig } from './footer';
+export { Header, defaultHeaderConfig } from './header';
+export { default as DropdownMenuSlot } from './menus/DropdownMenuSlot';
+export { default as LinkMenuItem } from './menus/LinkMenuItem';
