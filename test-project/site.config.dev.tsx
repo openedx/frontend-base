aa3b3291e4fa3aed573cbfcab0d3ba56f71fbce9
David Joy <davjoy@gmail.com>
2024-12-10 18:00:00 -0500

    test: updating test-project to the new format

--- test-project/site.config.dev.tsx
+++ test-project/site.config.dev.tsx
@@ -1,226 +1,234 @@
-import { createInternalAppConfig, PluginOperationTypes, PluginTypes } from '@openedx/frontend-base';
-
-import { EnvironmentTypes, ProjectSiteConfig } from '@openedx/frontend-base';
+import { defaultFooterConfig, defaultHeaderConfig, defaultShellConfig, EnvironmentTypes, ProjectSiteConfig } from '@openedx/frontend-base';
 import { authenticatedPageConfig, examplePageConfig, iframePluginConfig, pluginPageConfig } from './src';
-import ModularComponent from './src/plugin-page/components/ModularComponent';
-import PluginDirect from './src/plugin-page/components/PluginDirect';
 import './src/project.scss';
 
-const modifyWidget = (widget) => {
-  widget.content = {
-    title: 'Modified Modular Plugin',
-    uniqueText: 'Note that the original text defined in the JS config is replaced by this modified one.',
-  };
-  return widget;
-};
+// const modifyWidget = (widget) => {
+//   widget.content = {
+//     title: 'Modified Modular Plugin',
+//     uniqueText: 'Note that the original text defined in the JS config is replaced by this modified one.',
+//   };
+//   return widget;
+// };
 
-const modifyWidgetDefaultContentsUsernamePII = (widget) => {
-  widget.content = {
-    'data-custom-attr': 'customValue',
-    'data-another-custom-attr': '',
-    className: 'font-weight-bold',
-    style: { color: 'blue' },
-    onClick: (e) => {
-      console.log('Username clicked!', 'custom', e);
-    },
-  };
-  return widget;
-};
+// const modifyWidgetDefaultContentsUsernamePII = (widget) => {
+//   widget.content = {
+//     'data-custom-attr': 'customValue',
+//     'data-another-custom-attr': '',
+//     className: 'font-weight-bold',
+//     style: { color: 'blue' },
+//     onClick: (e) => {
+//       console.log('Username clicked!', 'custom', e);
+//     },
+//   };
+//   return widget;
+// };
 
-const modifyWidgetDefaultContentsLink = (widget) => {
-  widget.content.href = 'https://openedx.org';
-  return widget;
-};
+// const modifyWidgetDefaultContentsLink = (widget) => {
+//   widget.content.href = 'https://openedx.org';
+//   return widget;
+// };
 
-const wrapWidget = ({ component }) => (
-  <div className="bg-warning" data-testid="wrapper">
-    <div className="px-3">
-      <p className="mb-0">This is a wrapper component that is placed around the default content.</p>
-    </div>
-    {component}
-    <div className="px-3">
-      <p>With this wrapper, you can add anything before or after a component.</p>
-      <p className="mb-0">Note in the JS config that an iFrame plugin was Inserted, but a Hide operation was also used to hide it!</p>
-    </div>
-  </div>
-);
+// const wrapWidget = ({ component }) => (
+//   <div className="bg-warning" data-testid="wrapper">
+//     <div className="px-3">
+//       <p className="mb-0">This is a wrapper component that is placed around the default content.</p>
+//     </div>
+//     {component}
+//     <div className="px-3">
+//       <p>With this wrapper, you can add anything before or after a component.</p>
+//       <p className="mb-0">Note in the JS config that an iFrame plugin was Inserted, but a Hide operation was also used to hide it!</p>
+//     </div>
+//   </div>
+// );
 
 const config: ProjectSiteConfig = {
   apps: [
-    createInternalAppConfig('examplePage', examplePageConfig),
-    createInternalAppConfig('authenticatedPage', authenticatedPageConfig),
-    createInternalAppConfig('pluginPage', pluginPageConfig),
+    defaultShellConfig,
+    defaultHeaderConfig,
+    defaultFooterConfig,
+    examplePageConfig,
+    authenticatedPageConfig,
+    pluginPageConfig,
+    iframePluginConfig,
+    // createInternalAppConfig('examplePage', examplePageConfig),
+    // createInternalAppConfig('authenticatedPage', authenticatedPageConfig),
+    // createInternalAppConfig('pluginPage', pluginPageConfig),
 
-    // This site is serving its own iframe plugin and embedding it in the PluginPage.  This is
-    // obviously not quite like a real world use case, but it helps us test the mechanism without
-    // adding another test project to the library.
-    createInternalAppConfig('iframePlugin', iframePluginConfig),
+    // // This site is serving its own iframe plugin and embedding it in the PluginPage.  This is
+    // // obviously not quite like a real world use case, but it helps us test the mechanism without
+    // // adding another test project to the library.
+    // createInternalAppConfig('iframePlugin', iframePluginConfig),
   ],
 
   APP_ID: 'shell',
+  BASE_URL: 'http://apps.local.openedx.io:8080',
   ENVIRONMENT: EnvironmentTypes.DEVELOPMENT,
+  SITE_NAME: 'My Open edX Site',
+
+  // Frontend URLs
   ACCOUNT_PROFILE_URL: 'http://apps.local.openedx.io:1995',
   ACCOUNT_SETTINGS_URL: 'http://apps.local.openedx.io:1997',
-  BASE_URL: 'http://apps.local.openedx.io:8080',
   LEARNER_DASHBOARD_URL: 'http://local.openedx.io:8000/dashboard',
   LEARNING_BASE_URL: 'http://apps.local.openedx.io:2000',
-  LMS_BASE_URL: 'http://local.openedx.io:8000',
   LOGIN_URL: 'http://local.openedx.io:8000/login',
-  LOGOUT_URL: 'http://local.openedx.io:8000/logout',
+  ORDER_HISTORY_URL: 'http://apps.local.openedx.io:1996/orders',
   STUDIO_BASE_URL: 'http://studio.local.openedx.io:8001',
   MARKETING_SITE_BASE_URL: 'http://local.openedx.io:8000',
-  ORDER_HISTORY_URL: 'http://apps.local.openedx.io:1996/orders',
-  SITE_NAME: 'My Open edX Site',
+  LOGOUT_URL: 'http://local.openedx.io:8000/logout',
+
+  // API URLs
+  LMS_BASE_URL: 'http://local.openedx.io:8000',
   MFE_CONFIG_API_URL: 'http://apps.local.openedx.io:8080/api/mfe_config/v1',
 
+  // Brand URLs
   LOGO_URL: 'https://edx-cdn.org/v3/default/logo.svg',
   LOGO_TRADEMARK_URL: 'https://edx-cdn.org/v3/default/logo-trademark.svg',
   LOGO_WHITE_URL: 'https://edx-cdn.org/v3/default/logo-white.svg',
   FAVICON_URL: 'https://edx-cdn.org/v3/default/favicon.ico',
 
-  pluginSlots: {
-    slot_with_insert_operation: {
-      keepDefault: true,
-      plugins: [
-        {
-          op: PluginOperationTypes.INSERT,
-          widget: {
-            id: 'inserted_direct_plugin',
-            type: PluginTypes.DIRECT,
-            priority: 10,
-            RenderWidget: PluginDirect,
-          },
-        },
-        {
-          op: PluginOperationTypes.INSERT,
-          widget: {
-            id: 'inserted_iframe_plugin',
-            type: PluginTypes.IFRAME,
-            priority: 30,
-            url: 'http://apps.local.openedx.io:8080/iframe-plugin',
-            title: 'The iFrame plugin that is inserted in the slot',
-          },
-        },
-      ],
-    },
-    slot_with_modify_wrap_hidden_operations: {
-      keepDefault: true,
-      plugins: [
-        {
-          op: PluginOperationTypes.INSERT,
-          widget: {
-            id: 'inserted_plugin',
-            type: PluginTypes.DIRECT,
-            priority: 10,
-            RenderWidget: ModularComponent,
-            content: {
-              title: 'Modular Direct Plugin',
-              uniqueText: 'This is some text that will be replaced by the Modify operation below.',
-            },
-          },
-        },
-        {
-          op: PluginOperationTypes.INSERT,
-          widget: {
-            id: 'inserted_iframe_plugin',
-            type: PluginTypes.IFRAME,
-            priority: 30,
-            url: 'http://apps.local.openedx.io:8080/iframe-plugin',
-            title: 'This iFrame plugin will be hidden due to the Hide operation in this config.',
-          },
-        },
-        {
-          op: PluginOperationTypes.WRAP,
-          widgetId: 'default_contents',
-          wrapper: wrapWidget,
-        },
-        {
-          op: PluginOperationTypes.MODIFY,
-          widgetId: 'inserted_plugin',
-          fn: modifyWidget,
-        },
-        {
-          op: PluginOperationTypes.HIDE,
-          widgetId: 'inserted_iframe_plugin',
-        },
-      ],
-    },
-    slot_with_modular_plugins: {
-      keepDefault: true,
-      plugins: [
-        {
-          op: PluginOperationTypes.INSERT,
-          widget: {
-            id: 'insert_modular_direct_plugin',
-            type: PluginTypes.DIRECT,
-            priority: 1,
-            RenderWidget: ModularComponent,
-            content: {
-              title: 'Modular Direct Plugin',
-              uniqueText: 'This is a direct plugin with priority of 1, which is why it appears first in this slot.',
-            },
-          },
-        },
-      ],
-    },
-    slot_without_default: {
-      keepDefault: false,
-      plugins: [
-        {
-          op: PluginOperationTypes.INSERT,
-          widget: {
-            id: 'insert_direct_plugin',
-            type: PluginTypes.DIRECT,
-            priority: 1,
-            RenderWidget: ModularComponent,
-            content: {
-              title: 'Modular Direct Plugin With Content Defined in JS Config',
-              uniqueText: 'This modular component receives some of its content from the JS config (such as this text).',
-            },
-          },
-        },
-        {
-          op: PluginOperationTypes.INSERT,
-          widget: {
-            id: 'insert_another_direct_plugin',
-            type: PluginTypes.DIRECT,
-            priority: 10,
-            RenderWidget: PluginDirect,
-          },
-        },
-        {
-          op: PluginOperationTypes.INSERT,
-          widget: {
-            id: 'inserted_iframe_plugin',
-            type: PluginTypes.IFRAME,
-            priority: 30,
-            url: 'http://apps.local.openedx.io:8080/iframe-plugin',
-            title: 'The iFrame plugin that is inserted in the slot',
-          },
-        },
-      ],
-    },
-    slot_with_username_pii: {
-      keepDefault: true,
-      plugins: [
-        {
-          op: PluginOperationTypes.MODIFY,
-          widgetId: 'default_contents',
-          fn: modifyWidgetDefaultContentsUsernamePII,
-        },
-      ],
-    },
-    slot_with_hyperlink: {
-      keepDefault: true,
-      plugins: [
-        {
-          op: PluginOperationTypes.MODIFY,
-          widgetId: 'default_contents',
-          fn: modifyWidgetDefaultContentsLink,
-        },
-      ],
-    },
-  },
+  // pluginSlots: {
+  //   slot_with_insert_operation: {
+  //     keepDefault: true,
+  //     plugins: [
+  //       {
+  //         op: PluginOperationTypes.INSERT,
+  //         widget: {
+  //           id: 'inserted_direct_plugin',
+  //           type: PluginTypes.DIRECT,
+  //           priority: 10,
+  //           RenderWidget: PluginDirect,
+  //         },
+  //       },
+  //       {
+  //         op: PluginOperationTypes.INSERT,
+  //         widget: {
+  //           id: 'inserted_iframe_plugin',
+  //           type: PluginTypes.IFRAME,
+  //           priority: 30,
+  //           url: 'http://apps.local.openedx.io:8080/iframe-plugin',
+  //           title: 'The iFrame plugin that is inserted in the slot',
+  //         },
+  //       },
+  //     ],
+  //   },
+  //   slot_with_modify_wrap_hidden_operations: {
+  //     keepDefault: true,
+  //     plugins: [
+  //       {
+  //         op: PluginOperationTypes.INSERT,
+  //         widget: {
+  //           id: 'inserted_plugin',
+  //           type: PluginTypes.DIRECT,
+  //           priority: 10,
+  //           RenderWidget: ModularComponent,
+  //           content: {
+  //             title: 'Modular Direct Plugin',
+  //             uniqueText: 'This is some text that will be replaced by the Modify operation below.',
+  //           },
+  //         },
+  //       },
+  //       {
+  //         op: PluginOperationTypes.INSERT,
+  //         widget: {
+  //           id: 'inserted_iframe_plugin',
+  //           type: PluginTypes.IFRAME,
+  //           priority: 30,
+  //           url: 'http://apps.local.openedx.io:8080/iframe-plugin',
+  //           title: 'This iFrame plugin will be hidden due to the Hide operation in this config.',
+  //         },
+  //       },
+  //       {
+  //         op: PluginOperationTypes.WRAP,
+  //         widgetId: 'default_contents',
+  //         wrapper: wrapWidget,
+  //       },
+  //       {
+  //         op: PluginOperationTypes.MODIFY,
+  //         widgetId: 'inserted_plugin',
+  //         fn: modifyWidget,
+  //       },
+  //       {
+  //         op: PluginOperationTypes.HIDE,
+  //         widgetId: 'inserted_iframe_plugin',
+  //       },
+  //     ],
+  //   },
+  //   slot_with_modular_plugins: {
+  //     keepDefault: true,
+  //     plugins: [
+  //       {
+  //         op: PluginOperationTypes.INSERT,
+  //         widget: {
+  //           id: 'insert_modular_direct_plugin',
+  //           type: PluginTypes.DIRECT,
+  //           priority: 1,
+  //           RenderWidget: ModularComponent,
+  //           content: {
+  //             title: 'Modular Direct Plugin',
+  //             uniqueText: 'This is a direct plugin with priority of 1, which is why it appears first in this slot.',
+  //           },
+  //         },
+  //       },
+  //     ],
+  //   },
+  //   slot_without_default: {
+  //     keepDefault: false,
+  //     plugins: [
+  //       {
+  //         op: PluginOperationTypes.INSERT,
+  //         widget: {
+  //           id: 'insert_direct_plugin',
+  //           type: PluginTypes.DIRECT,
+  //           priority: 1,
+  //           RenderWidget: ModularComponent,
+  //           content: {
+  //             title: 'Modular Direct Plugin With Content Defined in JS Config',
+  //             uniqueText: 'This modular component receives some of its content from the JS config (such as this text).',
+  //           },
+  //         },
+  //       },
+  //       {
+  //         op: PluginOperationTypes.INSERT,
+  //         widget: {
+  //           id: 'insert_another_direct_plugin',
+  //           type: PluginTypes.DIRECT,
+  //           priority: 10,
+  //           RenderWidget: PluginDirect,
+  //         },
+  //       },
+  //       {
+  //         op: PluginOperationTypes.INSERT,
+  //         widget: {
+  //           id: 'inserted_iframe_plugin',
+  //           type: PluginTypes.IFRAME,
+  //           priority: 30,
+  //           url: 'http://apps.local.openedx.io:8080/iframe-plugin',
+  //           title: 'The iFrame plugin that is inserted in the slot',
+  //         },
+  //       },
+  //     ],
+  //   },
+  //   slot_with_username_pii: {
+  //     keepDefault: true,
+  //     plugins: [
+  //       {
+  //         op: PluginOperationTypes.MODIFY,
+  //         widgetId: 'default_contents',
+  //         fn: modifyWidgetDefaultContentsUsernamePII,
+  //       },
+  //     ],
+  //   },
+  //   slot_with_hyperlink: {
+  //     keepDefault: true,
+  //     plugins: [
+  //       {
+  //         op: PluginOperationTypes.MODIFY,
+  //         widgetId: 'default_contents',
+  //         fn: modifyWidgetDefaultContentsLink,
+  //       },
+  //     ],
+  //   },
+  // },
 
   custom: {
     FALSE_VALUE: false,
--- test-project/src/plugin-page/index.ts
+++ test-project/src/plugin-page/index.ts
@@ -1,7 +1,7 @@
-import { ApplicationModuleConfig } from '@openedx/frontend-base';
+import { App } from '@openedx/frontend-base';
 
-const config: ApplicationModuleConfig = {
-  route: {
+const config: App = {
+  routes: [{
     path: '/plugins',
     lazy: async () => {
       const { default: Component } = await import('./PluginPage');
@@ -9,7 +9,7 @@ const config: ApplicationModuleConfig = {
         Component,
       };
     },
-  }
+  }]
 };
 
 export default config;
