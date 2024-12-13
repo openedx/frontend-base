ebb7e0f0e66dd26b15a6e615d8fcb78adf896501
David Joy <davjoy@gmail.com>
2024-12-13 11:28:57 -0500

    fix: export ordering

--- shell/index.ts
+++ shell/index.ts
@@ -1,7 +1,7 @@
 export { default as DefaultLayout } from './DefaultLayout';
 export { default as DefaultMain } from './DefaultMain';
 export { default as defaultShellConfig } from './defaultShellConfig';
-export { Footer, defaultFooterConfig } from './footer';
-export { Header, defaultHeaderConfig } from './header';
+export { defaultFooterConfig, Footer } from './footer';
+export { defaultHeaderConfig, Header } from './header';
 export { default as LinkMenuItem } from './menus/LinkMenuItem';
 export { default as NavDropdownMenuSlot } from './menus/NavDropdownMenuSlot';
