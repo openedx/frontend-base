f09ac870e17fc52524b1de94665c1d330a040491
David Joy <davjoy@gmail.com>
2024-12-13 16:31:23 -0500

    refactor: renaming createInternalRoutes to getAppRoutes
    The function wasn’t creating anything, and it’s worth noting its about Apps.

--- shell/router/getAppRoutes.ts
+++ shell/router/getAppRoutes.ts
@@ -3,7 +3,7 @@ import { RouteObject } from 'react-router';
 import { getConfig } from '../../runtime';
 import { App } from '../../types';
 
-export default function createInternalRoutes() {
+export default function getAppRoutes() {
   const { apps } = getConfig();
 
   let routes: RouteObject[] = [];
