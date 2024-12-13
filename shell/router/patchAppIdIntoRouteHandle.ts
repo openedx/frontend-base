aa1d95dc98d0fd76913ec5e5909280953c1a87de
David Joy <davjoy@gmail.com>
2024-12-13 16:30:26 -0500

    fix: removing patchAppIdIntoRouteHandle
    This function is no longer used.

--- shell/router/patchAppIdIntoRouteHandle.ts
+++
@@ -1,9 +0,0 @@
-import { RouteObject } from 'react-router';
-
-// Save the appId in 'handle' so we can get it later.
-export default function patchAppIdIntoRouteHandle(appId: string, route: RouteObject) {
-  if (route.handle === undefined) {
-    route.handle = {};
-  }
-  route.handle.appId = appId;
-}
