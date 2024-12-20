0f498082e2e39656204cd5677cd9fdf403cb13d2
David Joy <davjoy@gmail.com>
2024-12-20 11:42:49 -0500

    test: adds some test cases for a few new slot operations

--- shell/dev-project/header/headerConfig.tsx
+++ shell/dev-project/header/headerConfig.tsx
@@ -19,8 +19,16 @@ const config: App = {
     },
     {
       slotId: 'frontend.shell.header.primaryLinks.widget',
+      id: 'header.learnerDashboard.linkAfter3',
+      op: WidgetOperationTypes.INSERT_AFTER,
+      relatedId: 'header.learnerDashboard.link3',
+      element: (<LinkMenuItem label="Link After 3" url="#" variant="navLink" />
+      )
+    },
+    {
+      slotId: 'frontend.shell.header.primaryLinks.widget',
       id: 'header.booyah.primaryLinks.dropdown',
-      op: WidgetOperationTypes.APPEND,
+      op: WidgetOperationTypes.PREPEND,
       element: (
         <NavDropdownMenuSlot id="frontend.shell.header.primaryLinks.dropdown.widget" label="Resources" />
       )
@@ -31,6 +39,20 @@ const config: App = {
       op: WidgetOperationTypes.APPEND,
       element: (
         <LinkMenuItem label="Resource 1" url="#" variant="dropdownItem" />
+      )
+    },
+    {
+      slotId: 'frontend.shell.header.primaryLinks.widget',
+      id: 'header.learnerDashboard.link3',
+      op: WidgetOperationTypes.APPEND,
+      element: (<LinkMenuItem label="Link 3" url="#" variant="navLink" />
+      )
+    },
+    {
+      slotId: 'frontend.shell.header.primaryLinks.widget',
+      id: 'header.learnerDashboard.link4',
+      op: WidgetOperationTypes.APPEND,
+      element: (<LinkMenuItem label="Link 4" url="#" variant="navLink" />
       )
     },
   ]
