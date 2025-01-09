9544ff6b3bb6a970781e7480bd502b96c12a0b6c
David Joy <davjoy@gmail.com>
2025-01-09 15:16:44 -0500

    test: adjusting names of test-project exports
    Still need to make a proper test setup for the iframe widget.

--- test-project/src/iframe-widget/IframeWidget.tsx
+++ test-project/src/iframe-widget/IframeWidget.tsx
@@ -1,10 +1,10 @@
 import { IFrameContentWrapper } from '@openedx/frontend-base';
 
-export default function PluginIframe() {
+export default function IframeWidget() {
   return (
     <IFrameContentWrapper>
       <section className="bg-light p-3 h-100">
-        <h4>Inserted iFrame Plugin</h4>
+        <h4>Inserted iFrame Widget</h4>
         <p>
           This is a component that lives in the test-project but is loaded into the page via an iframe.  This emulates a real-world scenario.  It is NOT testing for cross-origin security issues though, since it&apos;s on the same host name.
         </p>
