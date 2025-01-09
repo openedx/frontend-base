52e80a769e66a2317e8eefef196818a345b235b4
David Joy <davjoy@gmail.com>
2025-01-09 13:58:45 -0500

    refactor: exporting everything from config and constants in runtime
    Makes it simpler to maintain our export lists.

--- runtime/index.ts
+++ runtime/index.ts
@@ -31,25 +31,9 @@ export {
   setAuthenticatedUser
 } from './auth';
 
-export {
-  getConfig,
-  mergeConfig,
-  setConfig
-} from './config';
+export * from './config';
 
-export {
-  APP_ANALYTICS_INITIALIZED,
-  APP_AUTH_INITIALIZED,
-  APP_CONFIG_INITIALIZED,
-  APP_I18N_INITIALIZED,
-  APP_INIT_ERROR,
-  APP_LOGGING_INITIALIZED,
-  APP_PUBSUB_INITIALIZED,
-  APP_READY,
-  APP_TOPIC,
-  CONFIG_CHANGED,
-  CONFIG_TOPIC
-} from './constants';
+export * from './constants';
 
 export {
   configureI18n,
