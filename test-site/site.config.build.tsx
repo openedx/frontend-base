c7c2842340e0ce32c0727f9987cbf4e7a8517704
Adolfo R. Brandes <adolfo@axim.org>
2025-06-16 16:09:43 -0300

    fix: dev/test site configurations

--- test-site/site.config.build.tsx
+++ test-site/site.config.build.tsx
@@ -1,9 +1,19 @@
 import { defaultFooterConfig, defaultHeaderConfig, defaultShellConfig, EnvironmentTypes, SiteConfig } from '@openedx/frontend-base';
 
 import { authenticatedPageConfig, examplePageConfig, iframeWidgetConfig } from './src';
+
 import './src/site.scss';
 
-const config: SiteConfig = {
+const siteConfig: SiteConfig = {
+  siteId: 'test',
+  siteName: 'Test Site',
+  baseUrl: 'http://apps.local.openedx.io:8080',
+  lmsBaseUrl: 'http://local.openedx.io:8000',
+  loginUrl: 'http://local.openedx.io:8000/login',
+  logoutUrl: 'http://local.openedx.io:8000/logout',
+
+  environment: EnvironmentTypes.PRODUCTION,
+  mfeConfigApiUrl: 'http://apps.local.openedx.io:8080/api/mfe_config/v1',
   apps: [
     defaultShellConfig,
     defaultHeaderConfig,
@@ -12,14 +22,6 @@ const config: SiteConfig = {
     authenticatedPageConfig,
     iframeWidgetConfig,
   ],
-
-  environment: EnvironmentTypes.PRODUCTION,
-  baseUrl: 'http://localhost:8080',
-  lmsBaseUrl: 'http://localhost:18000',
-  loginUrl: 'http://localhost:18000/login',
-  logoutUrl: 'http://localhost:18000/logout',
-  siteName: 'localhost',
-  siteId: 'test',
 };
 
-export default config;
+export default siteConfig;
