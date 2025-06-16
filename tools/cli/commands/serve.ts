7e06fbf28d2c13ccd9dce7aa4a583219173410eb
Adolfo R. Brandes <adolfo@axim.org>
2025-06-16 16:08:43 -0300

    refactor: simplify serve command

--- tools/cli/commands/serve.ts
+++ tools/cli/commands/serve.ts
@@ -20,43 +20,20 @@ function isDirectoryEmpty(directoryPath: string) {
 const buildPath = path.join(process.cwd(), 'dist');
 const buildPathIndex = path.join(buildPath, 'index.html');
 
-const fallbackPort = 8080;
-
 if (isDirectoryEmpty(buildPath)) {
   const formattedBuildCmd = chalk.bold.redBright('``npm run build``');
   console.log(chalk.bold.red(`ERROR: No build found. Please run ${formattedBuildCmd} first.`));
 } else {
-  let configuredPort;
-
-  try {
-    configuredPort = require(path.join(process.cwd(), 'site.config.tsx'))?.PORT;
-  } catch (error) {
-    // Pass. Consuming applications may not have an `site.config.tsx` file. This is OK.
-  }
-
-  if (!configuredPort) {
-    configuredPort = process.env.PORT;
-  }
-
-  // No `PORT` found in `site.config.tsx` and/or `.env.development|private`, so output a warning.
-  if (!configuredPort) {
-    const formattedEnvDev = chalk.bold.yellowBright('.env.development');
-    const formattedEnvConfig = chalk.bold.yellowBright('site.config.tsx');
-    const formattedPort = chalk.bold.yellowBright(fallbackPort);
-    console.log(chalk.yellow(`No port found in ${formattedEnvDev} and/or ${formattedEnvConfig} file(s). Falling back to port ${formattedPort}.\n`));
-  }
-
   const app = express();
-
-  // Fallback to standard example port if no PORT config is set.
-  const PORT = configuredPort || fallbackPort;
 
   app.use(compression());
   app.use(express.static(buildPath));
-
   app.use('*', (req, res) => {
     res.sendFile(buildPathIndex);
   });
+
+  // Fallback to standard example port if no PORT config is set.
+  const PORT = process.env.PORT ?? 8080;
 
   app.listen(PORT, () => {
     const formattedServedFile = chalk.bold.cyanBright(buildPathIndex);
