ed6c0d4110d91d28b586fcd1f0c40a86f1628d44
Krish Tyagi <ktyagi-apphelix@2u.com>
2025-06-17 12:46:04 +0000

    fix!: [DEPR] Cleanup New Relic Source Map Webpack Plugin
    Removed new-relic-source-map-webpack-plugin imports/requires from all webpack configuration files. Cleaned up plugin configurations and related code. Removed any associated New Relic source map upload functionality

--- tools/webpack/modules.d.ts
+++ tools/webpack/modules.d.ts
@@ -1,6 +1,5 @@
 // This file needs to exist to declare modules for dependencies that don't publish their types.
 
 declare module 'postcss-custom-media';
-declare module '@edx/new-relic-source-map-webpack-plugin';
 declare module 'mini-css-extract-plugin';
 declare module 'webpack-bundle-analyzer';
