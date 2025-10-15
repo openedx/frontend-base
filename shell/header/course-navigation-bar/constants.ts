598f50d10b0fc8a15f5ae4b1a02f67ec7708421a
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-10-15 17:33:41 -0600

    feat: introduce courseNavigationBar

---
+++ shell/header/course-navigation-bar/constants.ts
@@ -0,0 +1,5 @@
+export const activeRolesForCourseNavigationBar = [
+  'org.openedx.frontend.role.learning',
+  'org.openedx.frontend.role.discussions',
+  'org.openedx.frontend.role.instructor',
+];
