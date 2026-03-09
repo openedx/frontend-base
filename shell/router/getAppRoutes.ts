5de4c0f9a3626ee6626bf1e758ae31dc4d15f24c
Jesus Balderrama <jesus.balderrama.wgu@gmail.com>
2026-03-09 08:50:27 -0600

    feat: validation in routes to redirect to login if user is not auth

--- shell/router/getAppRoutes.ts
+++ shell/router/getAppRoutes.ts
@@ -1,18 +1,48 @@
-import { RouteObject } from 'react-router';
+import React from 'react';
 
 import { getSiteConfig } from '../../runtime';
-import { App } from '../../types';
+import { App, RoleRouteObject } from '../../types';
+import AuthGuard from './AuthGuard';
+
+function createAuthenticatedComponent(OriginalComponent: React.ComponentType<any>) {
+  const AuthenticatedComponent: React.FC = (props) => {
+    return React.createElement(
+      AuthGuard,
+      {
+        requireAuthenticatedUser: true,
+      } as React.ComponentProps<typeof AuthGuard>,
+      React.createElement(OriginalComponent, props)
+    );
+  };
+
+  return AuthenticatedComponent;
+}
+
+function wrapRouteWithAuth(route: RoleRouteObject): RoleRouteObject {
+  // Check if route has requireAuthenticatedUser property using type assertion
+  const routeWithAuth = route as RoleRouteObject & { requireAuthenticatedUser?: boolean };
+  if (routeWithAuth.requireAuthenticatedUser && route.Component) {
+    return {
+      ...route,
+      Component: createAuthenticatedComponent(route.Component as React.ComponentType<any>),
+    };
+  }
+  return route;
+}
 
 export default function getAppRoutes() {
   const { apps } = getSiteConfig();
 
-  let routes: RouteObject[] = [];
+  let routes: RoleRouteObject[] = [];
 
   if (apps) {
     apps.forEach(
       (app: App) => {
         if (Array.isArray(app.routes)) {
-          routes = routes.concat(app.routes);
+          const wrappedRoutes = app.routes.map((route: RoleRouteObject) =>
+            wrapRouteWithAuth(route)
+          );
+          routes = routes.concat(wrappedRoutes);
         }
       }
     );
